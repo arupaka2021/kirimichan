63e9d54131e65c19580dba0ca645c1a603a6afdd
m111 <m111@skytree2021.site>
2021-06-14 16:03:33 +0900

    a

--- cgi-bin/foods_search.py
+++ cgi-bin/foods_search.py
@@ -1,6 +1,10 @@
 #!/usr/share/nginx/.virtualenvs/env3.7/bin/python
 import cgi, sys, io
 import get_food_data as dat
+import cgitb
+
+# エラーメッセージ表示
+cgitb.enable()
 
 # 文字化け対策
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
@@ -35,11 +39,12 @@ Content-type: text/html
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="utf-8">
-    <title>{title}</title>
+<meta charset="utf-8">
+<title>{title}</title>
 </head>
+
 <body>
-    <p></p>
+<p>{food}</p>
 </body>
 </html>
-'''[1:-1].format(title=title))
+'''[1:-1].format(title=title,food=food_list[0]))

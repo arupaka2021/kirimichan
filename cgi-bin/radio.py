3dc6405e8b5e6344a09d566c06fcc632ddacdcf8
m111 <m111@skytree2021.site>
2021-06-15 11:07:52 +0900

    カロリー合計

---
+++ cgi-bin/radio.py
@@ -0,0 +1,41 @@
+#!/usr/share/nginx/.virtualenvs/env3.7/bin/python
+
+import io, sys, cgi, cgitb
+
+# エラーメッセージ表示
+cgitb.enable()
+
+# 文字化け対策
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+title_str = 'ラジオボタン'
+
+form = cgi.FieldStorage()
+
+print('''
+Content-type: text/html
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>{title}</title>
+</head>
+
+<body>
+<form action="sum_calorie.py" method="post">
+	<p>メニュー1【必須】</p>
+	<label><input type="radio" name="menu1" value="hakumai" required>白米</label><br>
+	<label><input type="radio" name="menu1" value="genmai" required>玄米</label><br>
+	<label><input type="radio" name="menu1" value="takenoko" required>たけのこご飯</label><br>
+	<label><input type="radio" name="menu1" value="sekihan" required>赤飯</label><br>
+	<label><input type="radio" name="menu1" value="safuran" required>サフランライス</label><br>
+	<label><input type="radio" name="menu1" value="udon" required>うどん</label><br>
+	<label><input type="radio" name="menu1" value="somen" required>冷やしそうめん</label><br>
+	<label><input type="radio" name="menu1" value="others" required
+				onclick="connecttext('text_menu1',this.checked);">その他</label>：
+	<input type="text" name="othertext" id="text_menu1" value="">
+</form>
+</body>
+</html>
+'''[1:-1].format(title=title_str))
--- cgi-bin/sum_calorie.py
+++ cgi-bin/sum_calorie.py
@@ -1,3 +1,39 @@
-import menu as m, foods_search as fs
+#!/usr/share/nginx/.virtualenvs/env3.7/bin/python
+import cgi, sys, io
 
-sum_calorie = sum(m.menu1[])
+# 文字化け対策
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+form = cgi.FieldStorage()
+
+# 対象のフォーム変数名
+params = ['menu1',]
+
+# 結果を受け取る辞書
+r = {}
+
+for p in params:
+    if p in form:
+        r[p] = form[p].value
+    else:
+        r[p] = '(入力なし)'
+
+sum_calorie = r['menu1']
+
+title_str = 'カロリー合計'
+
+print('''
+Content-type: text/html
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>{title}</title>
+</head>
+
+<body>
+	<p>{sum_calorie}</p>
+</body>
+</html>
+'''[1:-1].format(title=title_str, sum_calorie=sum_calorie))

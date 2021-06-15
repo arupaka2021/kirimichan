d9120541e2f365296a88c0dc0859d7aca624e71f
NAKAMURA Shingo <git@itrev.info>
2021-06-16 07:20:17 +0900

    講師：画像アップロードファイル追加（未完成）

---
+++ cgi-bin/image_inst01.py
@@ -0,0 +1,62 @@
+#!/usr/share/nginx/.virtualenvs/env3.7/bin/python
+import cv2, cgi, sys, io, os, cgitb, datetime
+
+cgitb.enable()
+
+# 文字化け対策
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+form = cgi.FieldStorage()
+FORM_NAME = 'food_img'
+
+if FORM_NAME in form:
+    fileitem = form[FORM_NAME]
+
+    # ファイル名はアップロード時の時間から自動生成する（既存画像ファイルと重複するのを避けるため）
+    #now = datetime.datetime.now()
+    #fout = '../image/img'+now.strftime(%Y%m%d%H%M%S)
+    #with open(os.path.join('..\\image\\', form["food_img"].value + os.path.basename(fileitem.filename)), 'wb') as fout:
+    with open(os.path.join('..\\image', os.path.basename(fileitem.filename)), 'wb') as fout:
+        while 1:
+            chunk = fileitem.file.read(100000)
+            if not chunk: break
+            fout.write (chunk)
+        fout.close()
+
+    '''
+    ##### img = cv2.imread("C:/Users/Owner/Downloads/shiru1.jpeg")
+    # 「upload_img.html」から受け取った画像データ「food_img」を読み込み、img変数に格納
+    img = cv2.imread(r['food_img'])
+
+    # img変数に格納した画像データを指定したディレクトリに出力
+    img_name = "sample1.jpeg"
+    cv2.imwrite("../image/{img_name}".format(img_name="img_name"), img)
+    '''
+
+    print('''
+Content-type: text/html
+
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>画像アップロード完了</title>
+</head>
+
+<body>
+<h1>画像を{img_name}の名前で保存しました。</h1>
+</body>
+</html>
+'''[1:-1].format(img_name=img_name))
+
+else:
+    print('''
+Content-Type: text/html
+
+<html>
+<head></head>
+<body>
+<h1>Error</h1>
+<pre>{}</pre>
+</body>
+</html>
+'''[1:-1].format(type(form)))
